593f9679d2fff67edcbca6bc10147c5dcefb66ac
Eric Maike <ericmaikelopes@gmail.com>
2022-07-20 15:17:07 -0400

    Exercícios variáveis

---
+++ Fundamentos/Bloco 04.01/Variáveis.js
@@ -0,0 +1,18 @@
+const myName = "Eric"; 
+
+const birthCity = "Cacoal";
+
+let birthYear = 2004;
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+birthYear = 2030;
+
+console.log(birthYear)
+
+birthCity = "Rio de Janeiro"
+
+
+
