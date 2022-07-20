81c3629754092561e8c4f9aa9a82d11cbe0d5323
Eric Maike <ericmaikelopes@gmail.com>
2022-07-20 18:12:06 -0400

    Exercícios condições

---
+++ Fundamentos/Bloco 04.01/Script Condições If e Else.js
@@ -0,0 +1,21 @@
+const nota = 85;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+} 
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+} 
+else {
+    console.log("Você foi reprovada(o)"); 
+}
+
+
+let tempo = "ensolarado";
+
+if (tempo == "ensolarado") {
+    console.log("Irei a praia");
+} 
+else {
+    console.log("Não irei a praia");
+} 
